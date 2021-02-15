ef642f57ff4527039b669e73d9a8e4d558b4a6c1
Zachary Labe <zmlabe@rams.colostate.edu>
2021-02-15 10:07:52 -0700

    first commit

---
+++ Scripts/__init__.py
@@ -0,0 +1,6 @@
+'''
+The Python repository "ExtremeEvents" contains the scripts, data,
+figures, and text for investigating global climate model biases using large ensembles and neural networks. In particular, we focus on how unforced internal variability compares during the historical period. We are interested in using explainable AI for better understanding these nonlinear
+relationships.
+Author: Zachary Labe (zmlabe@rams.colostate.edu)
+'''
